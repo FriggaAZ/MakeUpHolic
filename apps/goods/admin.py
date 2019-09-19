546aedb04cdb8bbc79496e687b268dc828cc32e4
FriggaAZ <FriggaAZ@outlook.com>
2019-09-19 23:48:02 +0800

    首页静态化、页面缓存

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,10 +1,50 @@
 from django.contrib import admin
+from django.core.cache import cache
 from goods.models import GoodsType, GoodsSKU, IndexGoodsBanner, GoodsImage, IndexTypeGoodsBanner, IndexPromotionBanner
 # Register your models here.
 
-admin.site.register(GoodsType)
-admin.site.register(GoodsSKU)
-admin.site.register(IndexGoodsBanner)
-admin.site.register(GoodsImage)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(IndexPromotionBanner)
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''更新或新增数据时调用'''
+        super().save_model(request, obj, form, change)
+
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_sttic_index_html
+        generate_sttic_index_html.delay()
+
+        # 清除首页缓存
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        '''删除表中数据时调用'''
+        super().delete_model(request, obj)
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_sttic_index_html
+        generate_sttic_index_html.delay()
+
+        # 清除首页缓存
+        cache.delete('index_page_data')
+
+
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+# admin.site.register(GoodsSKU)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
+# admin.site.register(GoodsImage)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
